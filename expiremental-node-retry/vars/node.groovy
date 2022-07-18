1372f245037fb8563f1f08ddc19e5d496f79ddf9
Steve Boardwell <sboardwell@cloudbees.com>
2022-07-18 08:30:37 +0200

    Create (retry) node.groovy

---
+++ expiremental-node-retry/vars/node.groovy
@@ -0,0 +1,36 @@
+def call(String label, Closure body) {
+  call(['label': label, body])
+}
+
+def call(Map vars = [:], Closure body) {
+
+  int retries = vars.get('retries', 3) as int
+
+  // BODY
+  int attemptedRetries = 0
+  if (body) {
+    while (iteration < retries) {
+      try {
+        steps.invokeMethod('node', [label, body] as Object[])
+        break
+      } catch (x) {
+        if (nodeWasKilled(x)) {
+          if (attemptedRetries < retries) {
+            attemptedRetries++
+            continue
+          }
+          echo "Max retry attempts reached. Failing..."
+          throw x
+        } else {
+          throw x
+        }
+      }
+    }
+  }
+}
+
+@NonCPS
+def nodeWasKilled(def x) {
+  return x instanceof org.jenkinsci.plugins.workflow.steps.FlowInterruptedException &&
+  x.causes*.getClass().contains(org.jenkinsci.plugins.workflow.support.steps.ExecutorStepExecution.RemovedNodeCause)
+}
