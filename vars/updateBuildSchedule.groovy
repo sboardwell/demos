983c866efc0e7b4c8e0b832b512d16deffcb3cf4
Steve Boardwell <sboardwell@cloudbees.com>
2022-10-19 16:33:43 +0200

    Create updateBuildSchedule.groovy

---
+++ vars/updateBuildSchedule.groovy
@@ -0,0 +1,44 @@
+import hudson.model.*
+import hudson.triggers.*
+import org.jenkinsci.plugins.parameterizedscheduler.ParameterizedTimerTrigger
+
+def call(def vars = [:]) {
+  def jobStr = vars.jobName 
+  def cronStr = vars.cronStr
+  if (!jobStr || !cronStr) {
+    error "You must pass both jobStr and cronStr as variables"
+  }
+  
+  TriggerDescriptor TIMER_TRIGGER_DESCRIPTOR = Hudson.instance.getDescriptorOrDie(ParameterizedTimerTrigger.class)
+
+  def item = Jenkins.instance.getItemByFullName(jobStr)
+  if (!item) {
+    error "Job '${jobStr}' not found."
+  }
+  def timertrigger = item.getTriggers().get(TIMER_TRIGGER_DESCRIPTOR)
+  if (timertrigger) {
+    def actualSpec = timertrigger.parameterizedSpecification
+   if (cronStr.equals(actualSpec)) {
+     println "Cron spec identical ${getCronWithLineBreaks(cronStr)}. Doing nothing..."
+   } else {
+     println "Cron specs differ expected: ${getCronWithLineBreaks(cronStr)} vs actual: ${getCronWithLineBreaks(actualSpec)}. Changing..."
+     timertrigger = new ParameterizedTimerTrigger(cronStr)
+     item.addTrigger(timertrigger);
+     item.save();
+   }
+  } else {
+   println "Cron spec missing. Adding ${getCronWithLineBreaks(cronStr)}..."
+   timertrigger = new ParameterizedTimerTrigger(cronStr)
+   item.addTrigger(timertrigger);
+   item.save();
+  }
+  
+}
+
+def getCronWithLineBreaks(String cronStr) {
+   return """
+---
+${cronStr}
+---
+"""
+}
