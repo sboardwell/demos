7a220a7bcbf96a19eec5d8008f3c5cb8c81b410c
sboardwell <sboardwell@cloudbees.com>
2023-01-13 12:01:39 +0100

    Refactor to be truly @NonCPS

--- build-storm/vars/rebaseRepos.groovy
+++ build-storm/vars/rebaseRepos.groovy
@@ -2,23 +2,115 @@ import jenkins.branch.*
 import org.jenkinsci.plugins.workflow.multibranch.*
 
 def call() {
-    getFolderNames().each {
-        processFolder(it)
+    getFolderItemNamesAndNoTriggerProperty().each { orgFolderName, currentProperty ->
+        if (shouldProcessFolder(currentProperty.getBranches(), orgFolderName)) {
+            def propertyToReinstate = sanitizePropertyIfNeeded(currentProperty)
+            def tempProperty = new NoTriggerOrganizationFolderProperty('bla' + System.currentTimeMillis())
+            println """\
+                [INFO] : Current regex      : ${currentProperty.getBranches()}
+                [INFO] : Temp regex         : ${tempProperty.getBranches()}
+                [INFO] : Regex to reinstate : ${propertyToReinstate.getBranches()}
+                """.stripIndent()
+            try {
+                // set temp regex
+                println "[INFO] : Setting temporary NoTriggerOrganizationFolderProperty values for ${orgFolderName}... " + new Date()
+                replaceNoTriggerProperty(orgFolderName, currentProperty, tempProperty)
+
+                // trigger org folder scan
+                println "[INFO] : Waiting for the scan of '${orgFolderName}' to stop before scheduling..."
+                while (isBuildBlocked(orgFolderName, OrganizationFolder.class)) {
+                    sleep 1
+                    println "[INFO] : Still waiting for the scan of '${orgFolderName}' to stop before scheduling..."
+                }
+                println "[INFO] : Scan stopped! Scheduling the scan of '${orgFolderName}'..."
+                scheduleBuild(orgFolderName, OrganizationFolder.class)
+
+                println "[INFO] : Waiting for the scan of '${orgFolderName}' to start..."
+                while(!isBuildBlocked(orgFolderName, OrganizationFolder.class)) {
+                    sleep 1
+                    println "[INFO] : Still waiting for the scan of '${orgFolderName}' to start..."
+                }
+                println "[INFO] : Scan started! Waiting until it stops..."
+                while(isBuildBlocked(orgFolderName, OrganizationFolder.class)) {
+                    println "[INFO] : Still waiting for the scan of '${orgFolderName}' to stop..."
+                    sleep 1
+                }
+                println "[INFO] : Scan stopped!"
+
+                // iterate through child projects
+                println "[INFO MB] : Processing the Multibranch jobs from folder '${orgFolderName}'..."
+                getMultiBranchItemNames(orgFolderName).each { multiBranchItemName ->
+                    println "[INFO MB] : Waiting for the scan of '${multiBranchItemName}' to stop before scheduling..."
+                    while (isBuildBlocked(multiBranchItemName, WorkflowMultiBranchProject.class)) {
+                        sleep 1
+                        println "[INFO MB] : Still waiting for the scan of '${multiBranchItemName}' to stop before scheduling..."
+                    }
+                    println "[INFO MB] : Scan stopped! Scheduling the scan of '${multiBranchItemName}'..."
+                    scheduleBuild(multiBranchItemName, WorkflowMultiBranchProject.class)
+
+                    println "[INFO MB] : Waiting for the scan of '${multiBranchItemName}' to start..."
+                    while(!isBuildBlocked(multiBranchItemName, WorkflowMultiBranchProject.class)) {
+                        sleep 1
+                        println "[INFO MB] : Still waiting for the scan of '${multiBranchItemName}' to start..."
+                    }
+                    println "[INFO MB] : Scan started! Waiting until it stops..."
+                    while(isBuildBlocked(multiBranchItemName, WorkflowMultiBranchProject.class)) {
+                        println "[INFO MB] : Still waiting for the scan of '${multiBranchItemName}' to stop..."
+                        sleep 1
+                    }
+                    println "[INFO MB] : Scan stopped!"
+
+                    copyHashes(multiBranchItemName, WorkflowMultiBranchProject.class)
+                }
+            } finally {
+                println "[INFO] : Reinstating original NoTriggerOrganizationFolderProperty values for ${orgFolderName}... " + new Date()
+                replaceNoTriggerProperty(orgFolderName, tempProperty, propertyToReinstate)
+            }
+        }
     }
 }
 
 @NonCPS
-def getFolderNames() {
+def getItem(def fullName, def clazz) {
+    def item = Jenkins.instance.getAllItems(clazz).find { it.fullName == fullName}
+    if (item == null) {
+        error "Could not find item '${fullName}' of type '${clazz.name}'"
+    }
+    return item
+}
+
+@NonCPS
+def isBuildBlocked(def fullName, def clazz) {
+    return getItem(fullName, clazz).isBuildBlocked()
+}
+
+@NonCPS
+boolean scheduleBuild(def fullName, def clazz) {
+    if (!getItem(fullName, clazz).scheduleBuild(0)) {
+        error "Could not schedule build for '${fullName}' of type '${clazz.name}'"
+    }
+}
+
+@NonCPS
+def replaceNoTriggerProperty(String fullName, NoTriggerOrganizationFolderProperty actual, NoTriggerOrganizationFolderProperty replacement) {
+    def folder = getItem(fullName, OrganizationFolder.class)
+    folder.getProperties().remove(actual)
+    folder.addProperty(replacement)
+}
+
+@NonCPS
+def getFolderItemNamesAndNoTriggerProperty() {
     def folderItems = Jenkins.instance.getAllItems(OrganizationFolder.class)
-    def folderItemNames = []
+    def folderItemNamesAndNoTriggerProperty = [:]
     folderItems.each {
-        folderItemNames << it.fullName
+        NoTriggerOrganizationFolderProperty currentProperty = it.getProperties().get(NoTriggerOrganizationFolderProperty.class)
+        folderItemNamesAndNoTriggerProperty.put(it.fullName, currentProperty)
     }
-    return folderItemNames
+    return folderItemNamesAndNoTriggerProperty
 }
 
 @NonCPS
-boolean weShouldProcessFolder(String branchesRegex, String folderFullName) {
+boolean shouldProcessFolder(String branchesRegex, String folderFullName) {
     return (branchesRegex.startsWith('NEEDS_REBASING_PREFIX') || folderFullName == params?.ORG_FOLDER_NAME)
 }
 
@@ -37,59 +129,6 @@ def sanitizePropertyIfNeeded(def currentProperty) {
 }
 
 @NonCPS
-def triggerScan(def item, String infoPrefix = "[INFO]") {
-    println "${infoPrefix} : Waiting for the scan of '${item.fullName}' to stop before scheduling..."
-    while (item.isBuildBlocked()) {
-        Thread.sleep(1000) // sleep 1
-        println "${infoPrefix} : Still waiting for the scan of '${item.fullName}' to stop before scheduling..."
-    }
-    println "${infoPrefix} : Scan stopped!"
-
-    println "${infoPrefix} : Scheduling the scan of '${item.fullName}'..."
-    item.scheduleBuild(0)
-
-    println "${infoPrefix} : Waiting for the scan of '${item.fullName}' to start..."
-    while(!item.isBuildBlocked()) {
-        Thread.sleep(1000) // sleep 1
-        println "${infoPrefix} : Still waiting for the scan of '${item.fullName}' to start..."
-    }
-    println "${infoPrefix} : Scan started!"
-
-    println "${infoPrefix} : Scan the scan of '${item.fullName}' to start..."
-    while(item.isBuildBlocked()) {
-        println "${infoPrefix} : Waiting for the scan of '${item.fullName}' to stop..."
-        Thread.sleep(1000) // sleep 1
-    }
-    println "${infoPrefix} : Scan stop!"
-}
-
-@NonCPS
-def processFolder(String folderFullName) {
-    def folder = Jenkins.instance.getAllItems(OrganizationFolder.class).find { it.fullName == folderFullName}
-    println "[INFO] : Checking ${folder.fullName}... "
-    NoTriggerOrganizationFolderProperty currentProperty = folder.getProperties().get(NoTriggerOrganizationFolderProperty.class)
-    NoTriggerOrganizationFolderProperty propertyToReinstate = sanitizePropertyIfNeeded(currentProperty)
-    if (weShouldProcessFolder(currentProperty.getBranches(), folder.fullName)) {
-        String randomRegex = 'bla' + System.currentTimeMillis()
-        NoTriggerOrganizationFolderProperty tempProperty = new NoTriggerOrganizationFolderProperty(randomRegex)
-        println "[INFO] : Current regex : ${currentProperty.getBranches()}"
-        println "[INFO] : Temp regex    : ${tempProperty.getBranches()}"
-        println "[INFO] : Final regex   : ${propertyToReinstate.getBranches()}"
-        try {
-            folder.getProperties().remove(currentProperty)
-            folder.addProperty(tempProperty)
-            println "[INFO] : Scheduling : ${folder.fullName}..." + new Date()
-            triggerScan(folder)
-            processMultiBranchJobs(folder.fullName)
-        } finally {
-            println "[INFO] : Reinstating original NoTriggerOrganizationFolderProperty values for ${folder.fullName}... " + new Date()
-            folder.getProperties().remove(tempProperty)
-            folder.addProperty(propertyToReinstate)
-        }
-   }
-}
-
-@NonCPS
 def getMultiBranchItemNames(String folderFullName) {
     def multiBranchItems = Jenkins.instance.getAllItems(WorkflowMultiBranchProject.class)
     def multiBranchItemNames = []
@@ -101,18 +140,9 @@ def getMultiBranchItemNames(String folderFullName) {
     return multiBranchItemNames
 }
 
-void processMultiBranchJobs(String folderFullName) {
-    println "[INFO MB] : Processing the Multibranch jobs from folder '${folderFullName}'..."
-    getMultiBranchItemNames(folderFullName).each { multiBranchItemName ->
-        def multiBranchItem = Jenkins.instance.getAllItems(WorkflowMultiBranchProject.class).find { it.fullName == multiBranchItemName}
-        println "[INFO MB] : Scheduling : ${multiBranchItem.fullName}..." + new Date()
-        triggerScan(multiBranchItem, "[INFO MB]")
-        copyHashes(multiBranchItem.jobsDir)
-    }
-}
-
 @NonCPS
-def copyHashes(File parentDir) {
+def copyHashes(def jobName, def clazz) {
+    def parentDir = getItem(jobName, clazz).jobsDir
     parentDir.traverse(type: groovy.io.FileType.FILES, nameFilter: ~/scm-last-seen-revision-hash.xml/) { source ->
         def dest = new File(source.parent, 'scm-revision-hash.xml')
         dest << source.text
