e77a9358a9b106100904327c89db73714da29429
Steve Boardwell <sboardwell@cloudbees.com>
2023-03-23 13:58:10 +0100

    Update podTemplate.groovy

--- pod-template-test/vars/podTemplate.groovy
+++ pod-template-test/vars/podTemplate.groovy
@@ -1,13 +1,41 @@
 def call(def args, Closure body) {
-  echo "Steve's custom step running with: ${args}"
+  steps.echo "Custom podTemplate step running with: ${args}"
   String inheritFrom = args.inheritFrom
   def possibeMatches = []
   def logStr = []
-  // inheritFrom
-  if (inheritFrom) {
+  getInfos(inheritFrom, possibeMatches, logStr)
+  // check results
+  if (possibeMatches.size() == 0) {
+    logStr << "Nothing found, nothing to do..."
+  }
+  else if (possibeMatches.size() > 1) {
+    logStr << "Multiple matches found, don't know what to do..."
+  }
+  else {
+    def match = possibeMatches[0]
+    logStr << "Single match found, checking cloud and defaultContainer..."
+    if (!args.cloud) {
+      logStr << "Setting cloud... -> '${match.cloud}'"
+      args.cloud = match.cloud
+    } else {
+      logStr << "NOT setting cloud to '${match.cloud}'. Already set to -> '${args.cloud}'"
+    }
+    if (!args.defaultContainer && match.defaultContainer) {
+      logStr << "Setting defaultContainer -> '${match.defaultContainer}'..."
+      args.cloud = match.defaultContainer
+    } else {
+      logStr << "NOT setting cloud to '${match.cloud}'. Already set to -> '${args.cloud}'"
+    }
+  }
+  steps.echo logStr.join('\n')
+  steps.invokeMethod('podTemplate', [args, body] as Object[])
+}
+@NonCPS
+def getInfos(String label, def possibeMatches, def logStr) {
     def label = Label.get(inheritFrom)
     // check clouds for label
     label?.getClouds().each { cloud ->
+      def cloudInfos = [template:]
       // WARNING: the first template matching the label with be taken
       def template = cloud.getTemplate(label)
       String defaultContainer = ''
@@ -21,29 +49,4 @@ def call(def args, Closure body) {
       }
       possibeMatches.add([cloud: cloud.name, inheritFrom: inheritFrom, defaultContainer: defaultContainer])
     }
-    if (possibeMatches.size() == 0) {
-      logStr << "Nothing found, nothing to do..."
-    }
-    else if (possibeMatches.size() > 1) {
-      logStr << "Multiple matches found, don't know what to do..."
-    } 
-    else {
-      def match = possibeMatches[0]
-      logStr << "Single match found, checking cloud and defaultContainer..."
-      if (!args.cloud) {
-        logStr << "Setting cloud... -> '${match.cloud}'"
-        args.cloud = match.cloud
-      } else {
-        logStr << "NOT setting cloud to '${match.cloud}'. Already set to -> '${args.cloud}'"
-      }
-      if (!args.defaultContainer && match.defaultContainer) {
-        logStr << "Setting defaultContainer -> '${match.defaultContainer}'..."
-        args.cloud = match.defaultContainer
-      } else {
-        logStr << "NOT setting cloud to '${match.cloud}'. Already set to -> '${args.cloud}'"
-      }
-    }
-    echo logStr.join('\n')
-  }  
-  steps.invokeMethod('podTemplate', [args, body] as Object[])
 }
