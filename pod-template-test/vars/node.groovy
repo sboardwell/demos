af01229fc62d98cc6f91b8b688ec3a743ce68e02
Steve Boardwell <sboardwell@cloudbees.com>
2023-03-23 18:04:24 +0100

    Update node.groovy

--- pod-template-test/vars/node.groovy
+++ pod-template-test/vars/node.groovy
@@ -1,9 +1,9 @@
 def call(String label, Closure body) {
-  boolean nothingSetAndInStage = (script.env.POD_CONTAINER == null && script.env.STAGE_NAME)
-  if (script.env.POD_CONTAINER == null && script.env.STAGE_NAME) {
+  boolean nothingSetAndInStage = (env.POD_CONTAINER == null && env.STAGE_NAME)
+  if (env.POD_CONTAINER == null && env.STAGE_NAME) {
     String defaultContainerKey = "CUSTOM_DEFAULT_CONTAINER"
     String stageDefaultContainerKey = "CUSTOM_DEFAULT_CONTAINER_${STAGE_NAME}".replaceAll("[^A-Za-z0-9]", "_").toUpperCase()
-    String defaultContainer = script.env."${stageDefaultContainerKey}" ?: script.env."${defaultContainerKey} ? ""
+    String defaultContainer = env."${stageDefaultContainerKey}" ?: env."${defaultContainerKey} ? ""
     if (defaultContainer) {
       container(defaultContainer) {
         steps.invokeMethod('node', [label, body] as Object[])
