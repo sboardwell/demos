23a2556a39d96986931f9ebcacfd25a72cb13f29
Steve Boardwell <sboardwell@cloudbees.com>
2023-03-29 15:15:37 +0200

    Update node.groovy

--- pod-template-test/vars/node.groovy
+++ pod-template-test/vars/node.groovy
@@ -1,4 +1,5 @@
 def call(String label, Closure body) {
+  echo "Custom node step with label: ${label}"
   boolean nothingSetAndInStage = (env.POD_CONTAINER == null && env.STAGE_NAME)
   if (env.POD_CONTAINER == null && env.STAGE_NAME) {
     String defaultContainerKey = "CUSTOM_DEFAULT_CONTAINER"
@@ -10,9 +11,11 @@ def call(String label, Closure body) {
           body.call()
         }
       }
+      echo "Custom node step with label: ${label} - invoke newBody"
       steps.invokeMethod('node', [label, newBody] as Object[])
     }
   } else {
+    echo "Custom node step with label: ${label} - invoke current body"
     steps.invokeMethod('node', [label, body] as Object[])
   }
 }
